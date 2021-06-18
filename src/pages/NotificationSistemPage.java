0fd37d4c0af9ec06d602515a61e3dde3ef1178aa
Vucko1994 <markovuckovic1994@gmail.com>
2021-06-18 21:51:24 +0200

    Create NotificationSistemPage.java

---
+++ src/pages/NotificationSistemPage.java
@@ -0,0 +1,31 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class NotificationSistemPage extends BasicPage {
+
+	public NotificationSistemPage(WebDriver driver, JavascriptExecutor js) {
+		super(driver, js);
+	}
+
+	public WebElement getMessage() {
+		return driver.findElement(By.xpath("//*[contains(@class, 'alert--success')]"));
+	}
+
+	public String messageText() {
+		return driver.findElement(By.xpath("//div[@class=\"div_msg\"]/ul/li")).getText();
+	}
+
+	public void waitMessageToDisappear() {
+		WebDriverWait wait = new WebDriverWait(driver, 30);
+		WebElement Message = driver.findElement(By.xpath("//*[contains(@class, 'system_message')]"));
+
+		wait.until(ExpectedConditions.attributeToBe(Message, "style", "display: none;"));
+	}
+
+}
